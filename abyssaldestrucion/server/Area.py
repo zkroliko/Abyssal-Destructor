f5ba64a4c02e750a7324a095228e491e8b7e3e8a
Rafał <rafal.grm@gmail.com>
2016-04-20 15:53:55 +0200

    methods for checking area, submarine's position changing method

--- abyssaldestrucion/server/Area.py
+++ abyssaldestrucion/server/Area.py
@@ -9,5 +9,11 @@ class Area:
     WARN_Y0 = WARN_RATE*SIZE_Y
     WARN_Y1 = (1-WARN_RATE)*SIZE_Y
 
+    def is_valid_position(x, y):
+        return True if x > 0 and x < SIZE_X and y > 0 and y < SIZE_Y else False
+
+    def is_warning_position(x, y):
+        return True if x < WARN_X0 or x > WARN_X1 or y < WARN_Y0 or y > WARN_Y1 else False
+
     def __init__(self):
         pass
--- abyssaldestrucion/server/Sub.py
+++ abyssaldestrucion/server/Sub.py
@@ -6,10 +6,19 @@ from server.Area import Area
 class Sub:
     STEP_SIZE = 20.0
 
-    def __init__(self, x=None, y=None):
+    def __init__(self, area, x=None, y=None):
         if x is None or y is None:
+            self.area = area
             self.x = random.uniform(0, Area.SIZE_X)
             self.y = random.uniform(0, Area.SIZE_Y)
+
+    def change_position(self, x, y):
+        if self.area.is_valid_position(x, y):
+            self.x = x
+            self.y = y
+        else:
+            # raise some exception or do nothing
+        
 
     def change_orientation(self, value):
         pass
