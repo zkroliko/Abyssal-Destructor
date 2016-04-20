12c3f311e9f54a037d00e0989864f218af08e958
zkroliko <zbkrolikowski@gmail.com>
2016-04-20 15:43:23 +0200

    Some initial work

--- abyssaldestrucion/server/Area.py
+++ abyssaldestrucion/server/Area.py
@@ -6,6 +6,10 @@ class Area:
     WARN_RATE = 0.125
     WARN_X = WARN_RATE*SIZE_X
     WARN_Y = WARN_RATE*SIZE_Y
+    # Start margin
+    MARGIN_RATE = 0.2
+    MARGIN_X = MARGIN_RATE*SIZE_X
+    MARGIN_Y = MARGIN_RATE*SIZE_Y
 
     def __init__(self):
         pass
--- abyssaldestrucion/server/Sub.py
+++ abyssaldestrucion/server/Sub.py
@@ -1,17 +1,35 @@
 import random
 
+from numpy.core.umath import cos, sin
+
 from server.Area import Area
 
 
 class Sub:
     STEP_SIZE = 20.0
 
+    ORIENTATION_MIN = 0
+    ORIENTATION_MAX = 31
+    ORIENTATION_TO_ANGLE = 1
+
     def __init__(self, x=None, y=None):
         if x is None or y is None:
-            self.x = random.uniform(0, Area.SIZE_X)
-            self.y = random.uniform(0, Area.SIZE_Y)
+            self.x = random.uniform(Area.MARGIN_X, Area.SIZE_X-Area.MARGIN_X)
+            self.y = random.uniform(Area.MARGIN_Y, Area.SIZE_Y-Area.MARGIN_Y)
+        else:
+            self.x = x
+            self.y = y
+        self.angle = 0
+        self.angle_change = 0
 
     def change_orientation(self, value):
-        pass
+        val = min(self.ORIENTATION_MAX, max(self.ORIENTATION_MIN, value))
+        self.angle_change = self.__map_angle(val)
 
+    def move(self):
+        self.x += cos(self.angle)*self.STEP_SIZE
+        self.y += sin(self.angle)*self.STEP_SIZE
 
+    def __map_angle(self, angle):
+        return self.angle_change*self.ORIENTATION_TO_ANGLE
+        # TODO: Make mapping
