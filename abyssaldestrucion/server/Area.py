dae99de4248094c5e482d643ed4906a73e679934
Rafał <rafal.grm@gmail.com>
2016-04-20 15:16:29 +0200

    fixed coordinates for area warning

--- abyssaldestrucion/server/Area.py
+++ abyssaldestrucion/server/Area.py
@@ -2,10 +2,12 @@ class Area:
     # Sizes
     SIZE_X = 1000.0
     SIZE_Y = 1000.0
-    # Values for which the border waring should come up
+    # Values for which the border warning should come up
     WARN_RATE = 0.125
-    WARN_X = WARN_RATE*SIZE_X
-    WARN_Y = WARN_RATE*SIZE_Y
+    WARN_X0 = WARN_RATE*SIZE_X
+    WARN_X1 = (1-WARN_RATE)*SIZE_X
+    WARN_Y0 = WARN_RATE*SIZE_Y
+    WARN_Y1 = (1-WARN_RATE)*SIZE_Y
 
     def __init__(self):
         pass
