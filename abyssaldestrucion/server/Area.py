d5d84c9ef8b3b00f3f7eb57cbcc8e07287d0570e
zkroliko <zbkrolikowski@gmail.com>
2016-04-21 13:01:28 +0200

    Fix in area

--- abyssaldestrucion/server/Area.py
+++ abyssaldestrucion/server/Area.py
@@ -13,10 +13,10 @@ class Area:
     MARGIN_X = MARGIN_RATE*SIZE_X
     MARGIN_Y = MARGIN_RATE*SIZE_Y
 
-    def is_valid_position(x, y):
+    def is_valid_position(self, x, y):
         return True if 0 < x < Area.SIZE_X and 0 < y < Area.SIZE_Y else False
 
-    def is_warning_position(x, y):
+    def is_warning_position(self, x, y):
         return True if x < Area.WARN_X0 or x > Area.WARN_X1 or y < Area.WARN_Y0 or y > Area.WARN_Y1 else False
 
     def __init__(self):
--- abyssaldestrucion/tests/VisualiserTest.py
+++ abyssaldestrucion/tests/VisualiserTest.py
@@ -8,13 +8,17 @@ from server.Visualiser import Visualiser
 class TestSubClass(unittest.TestCase):
     def test_init(self):
         a = Area()
-        a.vessels.append(Sub(a))
-        a.vessels.append(Sub(a))
-        a.vessels.append(Sub(a))
+        sub1 = Sub(a)
+        sub2 = Sub(a)
+        sub3 = Sub(a)
+        a.vessels.append(sub1)
+        a.vessels.append(sub2)
+        a.vessels.append(sub3)
 
         v = Visualiser(a)
         v.start()
         for i in range(0, 100):
+            sub1.move()
             v.draw()
         v.master.mainloop()
 
